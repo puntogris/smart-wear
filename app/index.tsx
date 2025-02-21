0840601386aa138c98a5916fb93705302714eef8
puntogris <rosariodota3@gmail.com>
2025-02-21 16:01:10 -0300

    added location plugin

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,13 @@ import { Colors } from "@/constants/Colors";
 import { Link, Stack } from "expo-router";
 import { useEffect, useState } from "react";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
+import * as Location from "expo-location";
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
 
   useEffect(() => {
     fetch("https://smart-wear-sv.vercel.app/api/recs", {
@@ -18,10 +22,32 @@ export default function Home() {
       .then((data) => setData(data));
   }, []);
 
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+
+      const [geocodeResult] = await Location.reverseGeocodeAsync(
+        location.coords
+      );
+
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Stack.Screen options={{ headerShown: false }} />
       <Text>{JSON.stringify(data)}</Text>
+      <Text>{JSON.stringify(location)}</Text>
       <Link href="/settings">settings</Link>
     </SafeAreaView>
   );
