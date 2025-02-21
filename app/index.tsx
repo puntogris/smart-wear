4692595c6e19d0a7188d93d95f66205ddff558a0
puntogris <rosariodota3@gmail.com>
2025-02-21 09:21:06 -0300

    added api endpoint

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,27 @@
 import { Colors } from "@/constants/Colors";
 import { Link, Stack } from "expo-router";
+import { useEffect, useState } from "react";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://smart-wear-sv.vercel.app/api/recs", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "123",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Stack.Screen options={{ headerShown: false }} />
-      <Text>Home Screen</Text>
+      <Text>{JSON.stringify(data)}</Text>
       <Link href="/settings">settings</Link>
     </SafeAreaView>
   );
